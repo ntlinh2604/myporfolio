0ca619989877bb91b9c078d3264b30338929b63e
Thuy Linh Nguyen-Kinnel <ntlinh2604@gmail.con>
2022-11-27 19:47:55 +0100

    porfolio

---
+++ script.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
+  });
+
+  $(window).on('scroll load',function(){
+
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
+
+    if($(window).scrollTop() > 0){
+      $('.top').show();
+    }else{
+      $('.top').hide();
+    }
+
+  });
+
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500, 
+      'linear'
+    );
+
+  });
+
+});
+
+
+function myFunction() {
+
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (btnText.innerHTML === "see more projects") {
+    btnText.innerHTML = "see less projects"; 
+    moreText.style.display = "flex";
+  } else {
+    btnText.innerHTML = "see more projects"; 
+    moreText.style.display = "none";
+  }
+}
